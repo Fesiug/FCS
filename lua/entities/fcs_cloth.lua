6b2bc02a9b07a83f4ea96a95528d5bd72766f3f6
Fesiug <publicfesiug@outlook.com>
2024-01-28 13:35:59 -0500

    Junkshit fix for ragdolls & prep to junk this code

--- lua/entities/fcs_cloth.lua
+++ lua/entities/fcs_cloth.lua
@@ -35,36 +35,45 @@ function ENT:Think()
 end
 
 if CLIENT then
+	
+	local function LetsBoneMod( self, flags )
+		local bm = self:GetItemTable().BoneMods
+		if bm then
+			for k, v in pairs( bm ) do
+				if flags[k] then
+					for BoneName, BoneTask in pairs( v ) do
+						local BoneID = self:LookupBone( BoneName )
+						if !BoneID then continue end
+						local Matri = self:GetBoneMatrix( BoneID )
+						if !Matri then continue end
+						if BoneTask.scale then
+							Matri:Scale( BoneTask.scale )
+						end
+						if BoneTask.translate then
+							Matri:Translate( BoneTask.translate )
+						end
+						if BoneTask.rotate then
+							Matri:Rotate( BoneTask.rotate )
+						end
+						self:SetBoneMatrix( BoneID, Matri )
+					end
+				end
+			end
+		end
+	end
+
 	local v1 = Vector(1,1,1)
 	FCS_CSModelTable = FCS_CSModelTable or {}
 	function ENT:Draw()
 		local ply = self:GetFCSOwner()
 		if ply:IsValid() then
-			self:SetupBones()
-			local bm = self:GetItemTable().BoneMods
-			if bm then
-				for k, v in pairs( bm ) do
-					if ply:FCSGetFlags()[k] then
-						for BoneName, BoneTask in pairs( v ) do
-							local BoneID = self:LookupBone( BoneName )
-							if !BoneID then continue end
-							local Matri = self:GetBoneMatrix( BoneID )
-							if !Matri then continue end
-							if BoneTask.scale then
-								Matri:Scale( BoneTask.scale )
-							end
-							if BoneTask.translate then
-								Matri:Translate( BoneTask.translate )
-							end
-							if BoneTask.rotate then
-								Matri:Rotate( BoneTask.rotate )
-							end
-							self:SetBoneMatrix( BoneID, Matri )
-						end
-					end
-				end
-			end
+			-- Keep setting this in case of emergency
 			self:SetParent( ply )
+
+			-- Do bone modifications
+			self:SetupBones()
+			LetsBoneMod( self, ply:FCSGetFlags() )
+
 			local rge = ply:GetRagdollEntity()
 			local SUPERDEATH = ply:GetNW2Entity("SUPERDEATH", NULL)
 			if SUPERDEATH:IsValid() then
@@ -75,6 +84,7 @@ if CLIENT then
 					self.FakeClothes = ClientsideModel( self:GetModel() )
 					FCS_CSModelTable[self.FakeClothes] = true
 					self.FakeClothes.RGE = rge
+					self.FakeClothes.God = ply
 					self.FakeClothes:AddEffects( EF_BONEMERGE )
 					self.FakeClothes:AddEffects( EF_BONEMERGE_FASTCULL )
 					self.FakeClothes:SetParent( rge )
@@ -103,15 +113,51 @@ if CLIENT then
 			self.FakeClothes:Remove()
 		end
 	end
+
+	local debugg = CreateClientConVar( "fcs_debug_csmdl", 0, false, false )
+
+	local function dp( self, ... )
+		if debugg:GetBool() then
+			print( "[FCS] [CSMDL]", ... )
+		end
+	end
 	
 	hook.Add("Think", "FCS_Think_CSRagdoll", function()
 		for i, v in pairs( FCS_CSModelTable ) do
 			if !i:IsValid() then
 				FCS_CSModelTable[i] = nil
 				continue
+			else
+				if i:GetParent() == NULL then
+					if i.RGE:IsValid() then
+						i:SetParent( i.RGE )
+						dp( i, "Didn't have a parent before, have one now", i.RGE )
+					else
+						dp( i, "First check: I have no parent anymore and this is a disaster." )
+					end
+				end
+				if !i.RGE:IsValid() then
+					-- Get a new one
+					local rge = i.God:GetRagdollEntity()
+					local SUPERDEATH = i.God:GetNW2Entity("SUPERDEATH", NULL)
+					if SUPERDEATH:IsValid() then
+						rge = SUPERDEATH
+					end
+					if rge:IsValid() then
+						dp( i, "Got a new one ", rge )
+						i.RGE = rge
+						if i:GetParent() == NULL then
+							i:SetParent( i, i.RGE )
+							print( "Got a new parent", i:GetParent() )
+						end
+					else
+						dp( i, "Fail.. Remove" )
+						i:Remove()
+					end
+
+				end
 			end
 
-			if !i.RGE:IsValid() then i:Remove() end
 		end
 	end)
 end
