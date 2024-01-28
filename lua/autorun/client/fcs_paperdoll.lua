f9c125c3892b8d2eb7c6d762c87915060f4946d0
Fesiug <publicfesiug@outlook.com>
2024-01-28 14:03:37 -0500

    Several improvements for Paper Doll

--- lua/autorun/client/fcs_paperdoll.lua
+++ lua/autorun/client/fcs_paperdoll.lua
@@ -4,12 +4,43 @@ hook.Add("ShouldDrawLocalPlayer", "TEST_ShouldDrawLocalPlayer", function()
 end)
 
 local scaleme = 0.5
-hook.Add("Think", "TEST_Think", function()
-	local p = LocalPlayer()
-	scaleme = math.Approach( scaleme, input.IsKeyDown( KEY_MINUS ) and 0 or input.IsKeyDown( KEY_EQUAL ) and 1 or scaleme, FrameTime()/0.2 )
-end)
 
 local enable = CreateClientConVar("fcspaperdoll", 0, true, false)
+local clight = CreateClientConVar("fcspaperdoll_clight", 0, true, false)
+local zout = CreateClientConVar("fcspaperdoll_bind_zoomout", KEY_MINUS, true, false)
+local zin = CreateClientConVar("fcspaperdoll_bind_zoomin", KEY_EQUAL, true, false)
+
+local accel = 0
+
+hook.Add("Think", "TEST_Think", function()
+	local p = LocalPlayer()
+
+	local isit = input.IsKeyDown( zout:GetInt() ) or input.IsKeyDown( zin:GetInt() )
+	if isit then
+		accel = math.Approach( accel, 1, FrameTime() )
+	else
+		accel = 0
+	end
+	scaleme = math.Approach( scaleme, input.IsKeyDown( zout:GetInt() ) and 0 or input.IsKeyDown( zin:GetInt() ) and 1 or scaleme, FrameTime()/0.2*accel )
+end)
+
+local CustomLights = {
+	{
+		def_f = -32,
+		def_r = -64,
+		def_u = -64,
+		type = MATERIAL_LIGHT_POINT,
+		color = Vector(1, 1, 1)*-1.4,
+	},
+	{
+		def_f = 256,
+		def_r = 32,
+		def_u = 128,
+		type = MATERIAL_LIGHT_POINT,
+		color = Vector(1, 1, 1)*1.4,
+	},
+}
+
 
 hook.Add("HUDPaint", "TEST_HUDPaint", function()
 	if !enable:GetBool() then return end
@@ -34,7 +65,25 @@ hook.Add("HUDPaint", "TEST_HUDPaint", function()
 	cam.Start3D( e, a, Lerp( scaleme, 10, 4 ), x,y,w,h )
 		local wasiton = false
 		troll = true
-			--render.SuppressEngineLighting( true )
+		local cligh = clight:GetBool()
+			if cligh then
+				render.SuppressEngineLighting( true )
+				for i, v in ipairs( CustomLights ) do
+					v.pos = p:GetPos()
+
+					local nf = p:GetForward()
+					local nr = p:GetRight()
+					local nu = p:GetUp()
+					nf:Mul(v.def_f)
+					nr:Mul(v.def_r)
+					nu:Mul(v.def_u)
+					v.pos:Add( nf )
+					v.pos:Add( nr )
+					v.pos:Add( nu )
+					v.dir = -p:GetForward()
+				end
+				render.SetLocalModelLights(CustomLights)
+			end
 			p:DrawModel()
 
 			for i, v in ipairs( FCS.TL ) do
@@ -48,7 +97,7 @@ hook.Add("HUDPaint", "TEST_HUDPaint", function()
 				p:GetActiveWeapon():DrawModel()
 			end
 
-			--render.SuppressEngineLighting( false )
+			if cligh then render.SuppressEngineLighting( false ) end
 		troll = false
 		-- Flush ShouldDrawLocalPlayer cache, otherwise reload sounds and other stuff goes funny
 		cam.Start({})
@@ -56,30 +105,35 @@ hook.Add("HUDPaint", "TEST_HUDPaint", function()
 	cam.End3D()
 end)
 
-if CLIENT then
-	hook.Add("PopulateToolMenu", "FCS_PaperDoll_MenuOptions", function()
-		spawnmenu.AddToolMenuOption("Options", "Fesiug's Clothing Solutions", "FCS_PaperDoll", "Paper Doll", "", "", function( panel )
-			panel:AddControl("header", {
-				description = "Show a miniature portrait of your character.",
-			})
-			panel:AddControl("checkbox", {
-				label = "Enable Paper Doll",
-				command = "fcspaperdoll",
-			})
-			panel:ControlHelp("Several bugs from drawing the player right now, including:\n- Weird particle positions\n- Not hearing reload sounds\n- Odd first-person animations")
-			panel:ControlHelp("Press - and + to zoom in and out.")
-		end)
+hook.Add("PopulateToolMenu", "FCS_PaperDoll_MenuOptions", function()
+	spawnmenu.AddToolMenuOption("Options", "Fesiug's Clothing Solutions", "FCS_PaperDoll", "Paper Doll", "", "", function( panel )
+		panel:AddControl("header", {
+			description = "Show a miniature portrait of your character.",
+		})
+		panel:AddControl("checkbox", {
+			label = "Enable Paper Doll",
+			command = "fcspaperdoll",
+		})
+		panel:AddControl("checkbox", {
+			label = "Custom Lights",
+			command = "fcspaperdoll_clight",
+		})
+
+		panel:KeyBinder("Zoom Out", "fcspaperdoll_bind_zoomout")
+		panel:KeyBinder("Zoom In", "fcspaperdoll_bind_zoomin")
+
+		--panel:ControlHelp("Several bugs from drawing the player right now, including:\n- Weird particle positions\n- Not hearing reload sounds\n- Odd first-person animations")
 	end)
-	local firsttime = true
-	list.Set( "DesktopWindows", "FCS_PaperDoll_Icon", {
-		title = "FCS Paper Doll",
-		icon = "icon16/zoom.png",
-		init = function( icon, window )
-			GetConVar("fcspaperdoll"):SetBool( !GetConVar("fcspaperdoll"):GetBool() )
-			if firsttime and GetConVar("fcspaperdoll"):GetBool() then
-				chat.AddText( color_white, "Press - and + to zoom out and in." )
-				firsttime = false
-			end
+end)
+local firsttime = true
+list.Set( "DesktopWindows", "FCS_PaperDoll_Icon", {
+	title = "FCS Paper Doll",
+	icon = "icon16/zoom.png",
+	init = function( icon, window )
+		GetConVar("fcspaperdoll"):SetBool( !GetConVar("fcspaperdoll"):GetBool() )
+		if firsttime and GetConVar("fcspaperdoll"):GetBool() then
+			chat.AddText( color_white, "Press " .. input.GetKeyName( GetConVar("fcspaperdoll_bind_zoomout"):GetInt() ) .. " and " .. input.GetKeyName( GetConVar("fcspaperdoll_bind_zoomin"):GetInt() ) .. " to zoom out and in." )
+			firsttime = false
 		end
-	} )
-end
+	end
+} )
